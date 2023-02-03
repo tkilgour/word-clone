23becde9044d5ffb3df810f2a2be0030868540a0
Thomas Kilgour <thomas@kilgour.co>
2023-02-02 23:56:09 -0500

    update results key

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,8 +3,8 @@ import React from "react";
 function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess) => (
-        <p className="guess" key={guess}>
+      {guesses.map((guess, index) => (
+        <p className="guess" key={index}>
           {guess}
         </p>
       ))}
